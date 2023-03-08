466b647a248c3947389e27fd2591cf22fce15ee8
Lucasgojo <maunapoli34@gmail.com>
2023-03-08 10:47:39 -0300

    aulaum

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+const numeroUm = 0;
+var   numeroDois = 10;
+let   numeroTres = 20;
+
+//diferença entre cost e var 
+//diferença entre let e var
+
+console.log(numeroUm)
+//para que serve o console.log e como usá-lo.
+
+numeroUm = numeroDois;
+console .log(numeroUm)
+
+numeroUm = (numeroTres);
+console .log(numeroUm);
---
+++ index.js
@@ -0,0 +1,16 @@
+let semFlecha = false;
+let comFlecha = false;
+
+if (semFlecha) {
+    if (wanna) { 
+        console.log("Pegar Arco");
+    } else {
+        console.log("Pegarflecha");
+    }
+} else {
+     if (comFlecha) {
+        console.log("pegarEspada");
+     } else {
+        console.log("Nao usar arco");
+     }
+}
