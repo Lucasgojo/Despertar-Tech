1f7e65cd709bd3d80c42dbab9f1c9251bab3a9c5
Lucasgojo <maunapoli34@gmail.com>
2023-03-10 09:18:09 -0300

    CRIADAS PÁGINAS oficina

---
+++ oficina01.js
@@ -0,0 +1,17 @@
+
+let temDinheiro = true;
+let temSol = false;
+if (temDinheiro) {
+ if (temSol) {
+    console.log("Guarujá");
+ } else {
+    console.log("Restaurante");
+ }
+} else {
+    if (temSol) {
+        console.log("Ibirapuera");
+    } else {
+        console.log("Aula");
+    }
+}
+
